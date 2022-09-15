54ab11b4e6da2fe1c4686992d1b6e1f13a57f392
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-09-15 16:52:08 -0400

    Time: 61 ms (96.75%), Space: 15.1 MB (49.94%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,16 @@
 class Solution:
+    #Bruth Force 
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for left_index in range(len(nums)):
+    #         for right_index in range(left_index+1 , len(nums)): 
+    #             if nums[left_index] + nums[right_index] == target: 
+    #                 return [left_index, right_index]
+    #Alt solution
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for left_index in range(len(nums)):
-            for right_index in range(left_index+1 , len(nums)): 
-                if nums[left_index] + nums[right_index] == target: 
-                    return [left_index, right_index]
-        
+        hashtable = {} 
+        for i in range(len(nums)): 
+            hashtable[nums[i]] = i 
+        for i in range(len(nums)): 
+            complement = target -nums[i]
+            if complement in hashtable and hashtable[complement]!= i: 
+                return[i, hashtable[complement]]
