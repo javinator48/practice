3dd253751b3d44b3001ce01a02250b9718a75581
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-09-19 22:50:37 -0400

    Time: 308 ms (12.91%), Space: 14.7 MB (90.51%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,32 +4,45 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
 
         """
-        hashMap = {}
-        #print(matrix[0][0])
+        rows = len(matrix)
+        columns = len(matrix[0])
+        R, C = set(),set()
+        for i in range(rows): 
+            for j in range(columns): 
+                if matrix[i][j] == 0: 
+                    R.add(i)
+                    C.add(j)
+        for i in range(rows): 
+            for j in range(columns): 
+                if i in R or j in C: 
+                    matrix[i][j] = 0 
+#         hashMap = {}
+#         #print(matrix[0][0])
         
-        for index in range(len(matrix)): 
-            indices = []
-            for idx, value in enumerate(matrix[index]):
-                if value == 0: 
-                    indices.append(idx)
-            hashMap[index] = indices
-            #print(hashMap[index])
-        #rows 
-        for hashMapIndex in hashMap:
+#         for index in range(len(matrix)): 
+#             indices = []
+#             for idx, value in enumerate(matrix[index]):
+#                 if value == 0: 
+#                     indices.append(idx)
+#             hashMap[index] = indices
+#             #print(hashMap[index])
+#         #rows 
+#         for hashMapIndex in hashMap:
             
-            if  hashMap[hashMapIndex]: 
-                for zero_column_index in hashMap[hashMapIndex]: 
-                    for rowNumber in range(len(matrix)): 
-                        matrix[rowNumber][zero_column_index] = 0
+#             if  hashMap[hashMapIndex]: 
+#                 for zero_column_index in hashMap[hashMapIndex]: 
+#                     for rowNumber in range(len(matrix)): 
+#                         matrix[rowNumber][zero_column_index] = 0
                         
                 
                 
-                #replace whole row with 0 
-                #columns 
-                for index,value in enumerate(matrix[hashMapIndex]): 
+#                 #replace whole row with 0 
+#                 #columns 
+#                 for index,value in enumerate(matrix[hashMapIndex]): 
                    
                   
-                    matrix[hashMapIndex][index] = 0 
+#                     matrix[hashMapIndex][index] = 0 
+        
         
               
                     
