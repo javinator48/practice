a236a80f674306e90afe17c735afdabcfec71ad3
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-09-19 22:44:46 -0400

    Time: 151 ms (81.55%), Space: 14.8 MB (54.44%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -0,0 +1,44 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+
+        """
+        hashMap = {}
+        #print(matrix[0][0])
+        
+        for index in range(len(matrix)): 
+            indices = []
+            for idx, value in enumerate(matrix[index]):
+                if value == 0: 
+                    indices.append(idx)
+            hashMap[index] = indices
+            #print(hashMap[index])
+        #rows 
+        for hashMapIndex in hashMap:
+            
+            if  hashMap[hashMapIndex]: 
+                for zero_column_index in hashMap[hashMapIndex]: 
+                    for rowNumber in range(len(matrix)): 
+                        matrix[rowNumber][zero_column_index] = 0
+                        
+                
+                
+                #replace whole row with 0 
+                #columns 
+                for index,value in enumerate(matrix[hashMapIndex]): 
+                   
+                  
+                    matrix[hashMapIndex][index] = 0 
+        
+              
+                    
+        
+                
+            
+
+                
+            
+                   
+        
+        
