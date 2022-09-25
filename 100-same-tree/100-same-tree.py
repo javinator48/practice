4645f10108759c55c491c1a75ef4f0fd2d407c61
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-09-25 13:19:46 -0400

    Time: 65 ms (13.05%), Space: 13.9 MB (29.13%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -5,18 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # p and q both None: 
-        if not p and not q: 
-            return True 
-        # one of p and q is None
-        if not p or not q: 
-            return False 
+    #recursion
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         # p and q both None: 
+#         if not p and not q: 
+#             return True 
+#         # one of p and q is None
+#         if not p or not q: 
+#             return False 
     
-        #value is different
-        if p.val != q.val: 
-            return False 
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+#         #value is different
+#         if p.val != q.val: 
+#             return False 
+#         return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+    #iterative 
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def check(p,q): 
+            if not p and not q: 
+                return True 
+            if not p or not q: 
+                return False 
+            if p.val != q.val: 
+                return False 
+            return True
+        deq = deque([(p,q), ])
+        while deq: 
+            p, q = deq.popleft()
+            if not check(p,q):
+                return False 
+            if p: 
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+        return True 
+
         
         
         
