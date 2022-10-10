24d88559aab24d82bbe2f1554d243c58c3fdc000
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-10-09 23:42:32 -0400

    Time: 75 ms (5.61%), Space: 13.8 MB (65.21%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -3,9 +3,10 @@ class Solution:
         #empty case 
         if not nums: 
             return 0 
-        maxRobbedAmount = [None for _ in range(len(nums) + 1)]
+        maxRobbedAmount = [None for _ in range(len(nums)+1)]
         N = len(nums)
-        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
-        for i in range(N - 2, -1, -1):
-            maxRobbedAmount[i] = max(maxRobbedAmount[i + 1], maxRobbedAmount[i + 2] + nums[i])
-        return maxRobbedAmount[0]
+        maxRobbedAmount[0], maxRobbedAmount[1] = 0, nums[0]
+        for i in range(2, N+1): 
+            maxRobbedAmount[i] = max(maxRobbedAmount[i-2] + nums[i-1], maxRobbedAmount[i-1])
+      
+        return maxRobbedAmount[N]
