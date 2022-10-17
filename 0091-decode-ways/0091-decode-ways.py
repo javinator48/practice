3d2f603f448d0874ce8002219296dc3096ccbe8b
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-10-17 19:23:08 -0400

    Time: 34 ms (93.10%), Space: 13.9 MB (80.59%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,9 +1,11 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        #each element at an index represent number of ways to decode a word with length index 
         dp = defaultdict(int)
-        dp[-1] = 1
+        dp[-1] = 1 
+        
         is_valid = lambda x: x != '' and 1 <= int(x) <= 26 and x[0] != '0'
-        for i in range(len(s)):
+        for i in range(len(s)): 
             dp[i] += dp[i-1] if is_valid(s[i]) else 0 
             dp[i] += dp[i-2] if is_valid(s[i-1:i+1]) else 0 
-        return dp[len(s)-1]
+        return dp[len(s) -1]
