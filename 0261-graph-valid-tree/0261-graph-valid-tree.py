0b9b7ce00bf9ca2d3660e5a6c7387c56e6214f89
Javin Liu <46199601+javinator48@users.noreply.github.com>
2022-10-27 22:13:56 -0400

    Time: 152 ms (72.00%), Space: 16 MB (41.70%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,19 +1,34 @@
 class Solution:
+    def buildAdjacencyList(self, n, edges):
+		    adjList = [[] for _ in range(n)]
+		    for v1, v2 in edges:
+			    adjList[v1].append(v2)
+			    adjList[v2].append(v1)
+		    return adjList
+        
+
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        def find(u):
-            while u != parents[u]:
-                parents[u] = parents[parents[u]]
-                u = parents[u]
-            return u
+        #number of nodes not equal to number of edges  a tree has n-1 edges 
+        if n - 1 != len(edges): 
+            return False
+        #build an adjancy list from the edges list 
+        adjList = self.buildAdjacencyList(n, edges)
+        visited = set()
+        def hasCycle(v, parent): 
+            visited.add(v)
+            #traverse all neighbors 
+            for neighbor in adjList[v]: 
+         
+                if neighbor not in visited: 
+                    if hasCycle(neighbor, v): 
+                        return True 
+                elif neighbor != parent: 
+                    return True 
+            return False
+        # we only search with one vertex. 
+		# This helps to check if the graph is connected in the next step.
         
-        parents = [i for i in range(n)]
-        
-        for u,v in edges:
-            pu, pv = find(u), find(v)
-            
-            if pu == pv:
-                return False
-            
-            parents[pv] = pu
-        
-        return len({find(i) for i in range(n)}) == 1
+        if hasCycle(0, -1): 
+            return False
+        # If the graph is connected then all vertices must be visited
+        return True if len(visited) == n else False
